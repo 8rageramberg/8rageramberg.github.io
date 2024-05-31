bb66a81826d860399978367f0f67dad72e0b69d9
8rageramberg <brage.ramberg@gmail.com>
2024-05-31 17:09:14 +1000

    ADD to local storage

--- add.js
+++ add.js
@@ -1,9 +1,50 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Settings overlay
+    const addFolderForm = document.getElementById('addFolderForm');
+    const addBtn = document.getElementById('add-button');
 
-    // close buttons
 
+    addFolderForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const title = addFolderForm.querySelector('input[placeholder="Title"]').value;
+        const director = addFolderForm.querySelector('input[placeholder="Director"]').value;
+        const year = parseInt(addFolderForm.querySelector('input[placeholder="Year"]').value, 10);
+        const content = addFolderForm.querySelector('input[name="contentType"]:checked').value;
+        const genre = addFolderForm.querySelector('#genre').value;
+        const durationHours = parseInt(addFolderForm.querySelector('#durationHours').value, 10);
+        const durationMinutes = parseInt(addFolderForm.querySelector('#durationMinutes').value, 10);
+        const duration = `${durationHours}h ${durationMinutes}m`;
+        const watched = addFolderForm.querySelector('input[name="watched"]:checked').value === 'yes';
+        const rating = parseInt(addFolderForm.querySelector('#rating').value, 10);
+        const priority = parseInt(addFolderForm.querySelector('#priority').value, 10);
+        const imageUrl = addFolderForm.querySelector('#imgUrl').value;
+        const favorite = addFolderForm.querySelector('input[name="favourite"]').checked;
+
+        const film = {
+            title,
+            director,
+            year,
+            content,
+            genre,
+            duration,
+            watched,
+            rating,
+            priority,
+            imageUrl,
+            favorite,
+            date: new Date().toISOString()
+        };
+
+        const films = JSON.parse(localStorage.getItem('films')) || [];
+        films.push(film);
+        localStorage.setItem('films', JSON.stringify(films));
+        window.location.href = 'index.html';
+    });
+
+
+
+    // back buttons
     const backBtn = document.getElementById('back-button')
 
 
@@ -15,9 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (cmdKey && event.key === 'a') {
             event.preventDefault(); // Prevent default browser behavior
             backBtn.click();
-
         }
-
     });
 
 
@@ -26,7 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
         var footerHeight = document.querySelector('.footer').offsetHeight + 16;
         document.getElementById('page-extension').style.height = footerHeight + 'px';
     }
-
     window.addEventListener('load', adjustPageExtension);
 
+
 });
+
--- script.js
+++ script.js
@@ -1,123 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   let cardContainer = document.querySelector('.card-container');
   let markedCards = [];
-  const films = [
-    {
-      title: 'Batman Begins',
-      director: 'Christopher Nolan',
-      year: 2005,
-      content: 'Film',
-      genre: 'Fantasy / Action',
-      duration: '2h 20min',
-      watched: true,
-      rating: 9,
-      priority: 1,
-      favorite: true,
-      imageUrl: 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    {
-      title: 'The Dark Knight',
-      director: 'Christopher Nolan',
-      year: 2008,
-      content: 'Film',
-      genre: 'Fantasy / Action',
-      duration: '2h 32min',
-      watched: true,
-      rating: 9.2,
-      priority: 1,
-      favorite: true,
-      imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    {
-      title: 'Inception',
-      director: 'Christopher Nolan',
-      year: 2010,
-      content: 'Film',
-      genre: 'Sci-Fi / Action',
-      duration: '2h 28min',
-      watched: true,
-      rating: 8.8,
-      priority: 2,
-      favorite: true,
-      imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    {
-      title: 'Inception',
-      director: 'Christopher Nolan',
-      year: 2010,
-      content: 'Film',
-      genre: 'Sci-Fi / Action',
-      duration: '2h 28min',
-      watched: true,
-      rating: 8.8,
-      priority: 2,
-      favorite: true,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    {
-      title: 'Inception',
-      director: 'Christopher Nolan',
-      year: 2010,
-      content: 'Film',
-      genre: 'Sci-Fi / Action',
-      duration: '2h 28min',
-      watched: true,
-      rating: 8.8,
-      priority: 2,
-      favorite: true,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    {
-      title: 'Inception',
-      director: 'Christopher Nolan',
-      year: 2010,
-      content: 'Film',
-      genre: 'Sci-Fi / Action',
-      duration: '2h 28min',
-      watched: true,
-      rating: 8.8,
-      priority: 2,
-      favorite: true,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    {
-      title: 'Inception',
-      director: 'Christopher Nolan',
-      year: 2010,
-      content: 'Film',
-      genre: 'Sci-Fi / Action',
-      duration: '2h 28min',
-      watched: true,
-      rating: 8.8,
-      priority: 2,
-      favorite: true,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    {
-      title: 'Inception',
-      director: 'Christopher Nolan',
-      year: 2010,
-      content: 'Film',
-      genre: 'Sci-Fi / Action',
-      duration: '2h 28min',
-      watched: true,
-      rating: 8.8,
-      priority: 2,
-      favorite: true,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
-      date: new Date('2012-12-10T16:30:00')
-    },
-    // Add other film objects here as needed
-  ];
 
-  function renderFilms() {
+
+  const footer = document.querySelector('.footer');
+
+  // ADD overlay
+  const addBtn = document.getElementById('add-button');
+
+  // Settings overlay
+  const settingsBtn = document.querySelector('.settings-button');
+  const settingsOverlay = document.getElementById('settings');
+  const SettingsRestoreBtn = document.querySelector('.default-films')
+
+  // close buttons
+  const closeBtns = document.querySelectorAll('.close-button')
+
+  const films = JSON.parse(localStorage.getItem('films'));
+
+  function createCard() {
     cardContainer.innerHTML = '';
     films.forEach(film => {
 
@@ -126,11 +27,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const leftDiv = document.createElement('div');
       leftDiv.className = 'card-left';
+
       const rightDiv = document.createElement('div');
       rightDiv.className = 'card-right';
 
       const img = document.createElement('img');
       img.src = film.imageUrl;
+
+      // Handle image error
+      img.onerror = () => {
+        img.src = '';
+        img.alt = 'Image not found';
+
+        if (!leftDiv.querySelector('.error-msg')) {
+          const errorMsg = document.createElement('div');
+          errorMsg.textContent = 'Image not found';
+          errorMsg.style.color = 'red';
+          errorMsg.className = 'error-msg';
+          leftDiv.appendChild(errorMsg);
+        }
+      };
+
       img.alt = film.title;
       leftDiv.appendChild(img);
 
@@ -147,21 +64,24 @@ document.addEventListener('DOMContentLoaded', () => {
         <p><strong>Rating:</strong> ${film.rating !== null ? film.rating : 'None'}</p>
         <p><strong>Priority:</strong> ${film.priority}</p>
         <p><strong>Favorite:</strong> ${film.favorite ? 'Yes' : 'No'}</p>
-        <p><strong>Date:</strong> ${film.date.toLocaleString()}</p>
+       
       `;
+
+       // <p><strong>Date:</strong> ${film.date.toLocaleString()}</p>
       rightDiv.appendChild(details);
+
 
       filmCard.appendChild(leftDiv);
       filmCard.appendChild(rightDiv);
-
       cardContainer.appendChild(filmCard);
     });
   }
-  renderFilms();
+
+
+  createCard();
 
 
   // card action
-
   const cardMarker = document.querySelectorAll(".card");
   const delBtn = document.querySelector(".delete-button")
 
@@ -200,46 +120,38 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // Function to delete marked cards
   function deleteMarkedCards() {
     if (!markedCards || !Array.isArray(markedCards)) {
       console.log("Error: card container or marked cards not found.");
       return;
     }
+    // Get the indices of the marked cards
+    const markedIndices = markedCards.map(card => {
+      return Array.from(cardContainer.children).indexOf(card);
+    });
+
+    markedIndices.sort((a, b) => b - a).forEach(index => {
+        films.splice(index, 1);
+    });
 
     markedCards.forEach(card => {
-      // Remove the card from the DOM
       cardContainer.removeChild(card);
     });
+    markedCards = [];
+
+    // Update local storage
+    localStorage.setItem('films', JSON.stringify(films));
 
     // Clear the marked cards array
     markedCards = [];
-
-    // Re-render the films after deletion
-
   }
 
 
 
-  function editMarkedCard () {
+  function editMarkedCard() {
     console.log("Editing cards baby")
+
   }
-
-
-
-
-  const footer = document.querySelector('.footer');
-
-
-  // ADD overlay
-  const addBtn = document.getElementById('add-button');
-
-  // Settings overlay
-  const settingsBtn = document.querySelector('.settings-button');
-  const settingsOverlay = document.getElementById('settings');
-
-  // close buttons
-  const closeBtns = document.querySelectorAll('.close-button')
 
   // listeners
 
@@ -257,10 +169,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-
   settingsBtn.addEventListener('click', () => {
     settingsOverlay.classList.toggle('show');
     footer.classList.toggle('show');
+    SettingsRestoreBtn.addEventListener('click', restoreDefaultFilms)
   });
 
   window.addEventListener('click', (event) => {
@@ -308,11 +220,193 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('page-extension').style.height = footerHeight + 'px';
   }
 
+
+
+
+
   window.addEventListener('load', adjustPageExtension);
 
   delBtn.addEventListener("click", deleteMarkedCards);
 
   optionsToggle.addEventListener('click', toggleOptions);
+
+
+  function restoreDefaultFilms() {
+    const films = [
+      {
+        title: 'Batman Begins',
+        director: 'Christopher Nolan',
+        year: 2005,
+        content: 'Film',
+        genre: 'Fantasy / Action',
+        duration: '2h 20min',
+        watched: true,
+        rating: 9,
+        priority: 1,
+        favorite: true,
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg',
+
+      },
+      {
+        title: 'The Dark Knight',
+        director: 'Christopher Nolan',
+        year: 2008,
+        content: 'Film',
+        genre: 'Fantasy / Action',
+        duration: '2h 32min',
+        watched: true,
+        rating: 9.2,
+        priority: 1,
+        favorite: true,
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg',
+
+      },
+      {
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: 2010,
+        content: 'Film',
+        genre: 'Sci-Fi / Action',
+        duration: '2h 28min',
+        watched: true,
+        rating: 8.8,
+        priority: 2,
+        favorite: true,
+        imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg',
+
+      },
+      {
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: 2010,
+        content: 'Film',
+        genre: 'Sci-Fi / Action',
+        duration: '2h 28min',
+        watched: true,
+        rating: 8.8,
+        priority: 2,
+        favorite: true,
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
+
+      },
+      {
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: 2010,
+        content: 'Film',
+        genre: 'Sci-Fi / Action',
+        duration: '2h 28min',
+        watched: true,
+        rating: 8.8,
+        priority: 2,
+        favorite: true,
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
+
+      },
+      {
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: 2010,
+        content: 'Film',
+        genre: 'Sci-Fi / Action',
+        duration: '2h 28min',
+        watched: true,
+        rating: 8.8,
+        priority: 2,
+        favorite: true,
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
+
+      },
+      {
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: 2010,
+        content: 'Film',
+        genre: 'Sci-Fi / Action',
+        duration: '2h 28min',
+        watched: true,
+        rating: 8.8,
+        priority: 2,
+        favorite: true,
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
+
+      },
+      {
+        title: 'Inception',
+        director: 'Christopher Nolan',
+        year: 2010,
+        content: 'Film',
+        genre: 'Sci-Fi / Action',
+        duration: '2h 28min',
+        watched: true,
+        rating: 8.8,
+        priority: 2,
+        favorite: true,
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Lord_Rings_Fellowship_Ring.jpg',
+
+      },
+
+    ];
+
+    films.forEach(film => {
+
+      const filmCard = document.createElement('div');
+      filmCard.className = 'card';
+
+      const leftDiv = document.createElement('div');
+      leftDiv.className = 'card-left';
+
+      const rightDiv = document.createElement('div');
+      rightDiv.className = 'card-right';
+
+      const img = document.createElement('img');
+      img.src = film.imageUrl;
+
+      // Handle image error
+      img.onerror = () => {
+        img.src = '';
+        img.alt = 'Image not found';
+
+        if (!leftDiv.querySelector('.error-msg')) {
+          const errorMsg = document.createElement('div');
+          errorMsg.textContent = 'Image not found';
+          errorMsg.style.color = 'red';
+          errorMsg.className = 'error-msg';
+          leftDiv.appendChild(errorMsg);
+        }
+      };
+
+      img.alt = film.title;
+      leftDiv.appendChild(img);
+
+      const details = document.createElement('div');
+      details.className = 'card-details';
+      details.innerHTML = `
+        <h2>${film.title}</h2>
+        <p><strong>Director:</strong> ${film.director}</p>
+        <p><strong>Year:</strong> ${film.year}</p>
+        <p><strong>Content:</strong> ${film.content}</p>
+        <p><strong>Genre:</strong> ${film.genre}</p>
+        <p><strong>Duration:</strong> ${film.duration}</p>
+        <p><strong>Watched:</strong> ${film.watched ? 'Yes' : 'No'}</p>
+        <p><strong>Rating:</strong> ${film.rating !== null ? film.rating : 'None'}</p>
+        <p><strong>Priority:</strong> ${film.priority}</p>
+        <p><strong>Favorite:</strong> ${film.favorite ? 'Yes' : 'No'}</p>
+        <p><strong>Date:</strong> ${film.date.toLocaleString()}</p>
+      `;
+      rightDiv.appendChild(details);
+
+
+      filmCard.appendChild(leftDiv);
+      filmCard.appendChild(rightDiv);
+      cardContainer.appendChild(filmCard);
+
+      const films = JSON.parse(localStorage.getItem('films')) || [];
+      films.push(film);
+      localStorage.setItem('films', JSON.stringify(films));
+      window.location.href = 'index.html';
+    });
+  }
 
 
 });
