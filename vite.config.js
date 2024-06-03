518b94e88d7a6bb33d076bfd3a1da2d11c93e7cf
8rageramberg <brage.ramberg@gmail.com>
2024-06-03 10:49:49 +1000

    fix

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<repository-name>/', // Replace <repository-name> with your actual repository name
+  base: '/', // Replace <repository-name> with your actual repository name
 });
